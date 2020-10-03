487a5595e8aaa56e8b4658e57814a7dacf92919e
Anna Zhang <annazhang2002@gmail.com>
2020-10-02 22:13:36 -0700

    finished part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Anna Zhang on 9/26/20.
+//  Copyright © 2020 Anna Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
